b8957bd5efd7ca66044cc23ccc67071899c80c40
viCodexm <uinkognito@yandex.ru>
2023-11-17 11:22:20 +0300

    solve graph

---
+++ Algorithms and data structures/lab5 graph/graph.cpp
@@ -0,0 +1,59 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//#define DEBUG 0
+
+struct edge {
+    set<int> to;
+    set<int> in_me;
+};
+
+int main() {
+    
+    #ifdef DEBUG
+        freopen("test.txt", "r", stdin);
+    #endif
+
+    cout << "Введите n и k:\n";
+    int n, k; cin >> n >> k;
+    cout << "Введите k пар from, to:\n";
+    unordered_map<int, edge> graph;
+    for (int i = 0; i < k; ++i) {
+        int from, to; cin >> from >> to;
+        graph[from].to.insert(to);
+        graph[to].in_me.insert(from);
+    }
+    // add edges that previously were not condisered
+    for (int i = 1; i <= n; ++i) 
+        graph[i];
+
+    vector<int> ans;
+    while (true) {
+        vector<int> del;
+        for (auto g : graph) {
+            auto& [idx, links] = g;
+            
+            if (links.in_me.empty()) {
+                for (int to : links.to) {
+                    graph[to].in_me.erase(idx);
+                }
+                del.push_back(idx);
+            }
+        }
+        for (int& idx : del) {
+            ans.push_back(idx);
+            graph.erase(idx);
+        }
+
+        if (del.empty()) {
+            cout << "Вывод частичного графа:\n";
+            break;
+        }
+    }
+    for (int& e : ans)
+        cout << e << " ";
+
+    return 0;
+}
