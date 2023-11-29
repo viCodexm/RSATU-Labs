4b7eb002c4f2c3bc7c1685152b29b218d45df7e9
viCodexm <uinkognito@yandex.ru>
2023-11-30 00:03:11 +0300

    currently solving var 2

---
+++ Computing systems architecture/lab4/var 2/converter.hpp
@@ -0,0 +1,233 @@
+#pragma once
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MINUS_ZERO_DOESNT_EXIST "Значения '-0' в данном коде не существует"
+
+string code_reverse(string code) {
+
+    for (int i = 0; i < code.size(); ++i)
+        (code[i] == '1') ? code[i] = '0' : code[i] = '1';
+
+    return code;
+}
+string code_add_one(string code) {
+    int i = code.size() - 1;
+    code[i]++;
+    bool carry = code[i] == '2';
+    while (carry) {
+        code[i] -= 2;
+        i--;
+        code[i]++;
+        carry = code[i] == '2';
+    }
+    return code;
+}
+// assume that a.size == b.size
+string code_plus_code(string a, string b) {
+    int carry = 0;
+    for (int i = a.size() - 1; i >= 0; --i) {
+        if (a[i] == '.')
+            continue;
+        
+        int count = ((a[i] == '1') + (b[i] == '1')) + min(1, carry);
+
+        switch (count) {
+        case 0:
+            break;
+        case 1:
+            if (carry)
+                carry = false;
+            a[i] = '1';
+            break;
+        case 2:
+            carry = true;
+            a[i] = '0';
+            break;
+        case 3:
+            carry = true;
+            a[i] = '1';
+            break;
+        }
+    }
+    return (carry) ? "00" + a.substr(2) : a;
+}
+string mdk_code_move_left(string code) {
+    code += '0'; swap(code[2], code[3]);
+    return code.substr(1);
+}
+
+void make_same_size(string& a, string& b, int prefix_length) {
+    int asize = a.size(), bsize = b.size();
+    const int diff = abs(asize - bsize);
+    if (asize < bsize)
+        a = a.substr(0, prefix_length) + string(diff, '0') + a.substr(prefix_length, asize);
+    else if (asize > bsize)
+        b = b.substr(0, prefix_length) + string(diff, '0') + b.substr(prefix_length, bsize);
+}
+
+
+
+// код из второй лабы
+#define MINUS_ZERO_DOESNT_EXIST "Значения '-0' в данном коде не существует"
+
+// val, "val < 0", force_any_size (0 - dont)
+string to_straight_code(int val, bool negative, int body_size) {
+    if (val == 0)
+        return negative ? "1.0" : "0.0";
+
+    string s;
+    int n = abs(val);
+    while (n > 0) {
+        s += to_string(n % 2);
+        n /= 2;
+    }
+    reverse(s.begin(), s.end());
+
+    if (s.size() >= body_size) {
+        return (val >= 0) ? "0." + s : "1.0" + s;
+    }
+    else {
+        int add = body_size - s.size();
+        return (val >= 0) ? "0." + string(add, '0') + s : "1." + string(add, '0') + s;
+    }
+
+    return s;
+}
+string to_reverse_code(int val, bool negative, int body_size) {
+    if (val == 0 && negative)
+        return MINUS_ZERO_DOESNT_EXIST;
+    
+    string s = to_straight_code(val, negative, body_size);
+    if (val >= 0)
+        return s;
+
+    for (int i = 2; i < s.size(); ++i)
+        if (s[i] == '1')
+            s[i] = '0';
+        else if (s[i] == '0')
+            s[i] = '1';
+    return s;
+}
+string to_extended_code(int val, bool negative, int body_size) {
+    if (val == 0 && negative)
+        return MINUS_ZERO_DOESNT_EXIST;
+
+    string s = to_reverse_code(val, negative, body_size);
+    if (val >= 0)
+        return s;
+
+    int i = s.size() - 1;
+    s[i]++;
+    bool carry = s[i] == '2';
+    while (carry) {
+        s[i] -= 2;
+        i--;
+        if (s[i] == '.')
+            break;
+        s[i]++;
+        carry = s[i] == '2';
+    }
+    return s;
+}
+string to_mod_extended_code(int val, bool negative, int body_size) {
+    if (val == 0 && negative)
+        return MINUS_ZERO_DOESNT_EXIST;
+
+    string s = to_extended_code(val, negative, body_size);
+    
+    if (val >= 0) return "00." + s.substr(2, s.size() - 1);
+    return "11." + s.substr(2, s.size() - 1);
+}
+
+int binary_to_int(string code) {
+    int sign = (code[0] == '1' || code[1] == '1') ? -1 : 1;
+    reverse(code.begin(), code.end());
+    const int size = code.size();
+    int ans = 0;
+    for (int i = 0; i < size && code[i] != '.'; ++i)
+        if (code[i] == '1')
+            ans += pow(2, i);
+    
+    return sign * ans;
+}
+
+// separates every 4 chars in string with space
+string space_wrapper(string output) {
+    string str;
+    int idx = 0;
+    for (char& c : output) {
+        if (idx == 4) {
+            idx = 0;
+            str += " ";
+        }
+        str += c;
+        idx++;
+    }
+    return str;
+}
+
+bool isHex(string str) {
+    if (str.size() != 8)
+        return false;
+    for (char& c : str)
+        if (not(('A' <= c && c <= 'Z') || isdigit(c)))
+            return true;
+    return true;
+}
+string hex_to_bin(string hexStr) {
+    map<char, string> hexMap = {
+        {'0', "0000"}, {'1', "0001"}, {'2', "0010"}, {'3', "0011"},
+        {'4', "0100"}, {'5', "0101"}, {'6', "0110"}, {'7', "0111"},
+        {'8', "1000"}, {'9', "1001"}, {'A', "1010"}, {'B', "1011"},
+        {'C', "1100"}, {'D', "1101"}, {'E', "1110"}, {'F', "1111"}
+    };
+
+    string binStr = "";
+    for (char c : hexStr)
+        binStr += hexMap[c];
+
+    return binStr;
+}
+string bin_to_hex(string binStr) {
+    map<string, char> binMap = {
+        {"0000", '0'}, {"0001", '1'}, {"0010", '2'}, {"0011", '3'},
+        {"0100", '4'}, {"0101", '5'}, {"0110", '6'}, {"0111", '7'},
+        {"1000", '8'}, {"1001", '9'}, {"1010", 'A'}, {"1011", 'B'},
+        {"1100", 'C'}, {"1101", 'D'}, {"1110", 'E'}, {"1111", 'F'}
+    };
+    string hexStr = "";
+    for (int i = 0; i < binStr.size() - 3; i += 4)
+        hexStr += binMap[binStr.substr(i, 4)];
+    return hexStr;
+}
+int bin_to_dec(string binStr) {
+    int decimal = 0;
+    const int size = binStr.size();
+    for (int i = 0; i < size; ++i) {
+        if (binStr[i] == '1')
+            decimal += pow(2, size - i - 1);
+    }
+    return decimal;
+}
+
+string get_matisse(string binStr) {
+    string mantisse = "0," + binStr.substr(9, 32);
+    while (!mantisse.empty() && mantisse[mantisse.size() - 1] == '0')
+        mantisse.pop_back();
+    return mantisse;
+}
+string without_leading_zeroes(string str) {
+    int leading_zeros = 0;
+    while (leading_zeros < str.size() && str[leading_zeros] == '0')
+        leading_zeros++;
+    return str.substr(leading_zeros);
+}
+
+string with_comma(string str) {
+    if (str[1] == '.')
+        str.insert(3, 1, ',');
+    else str.insert(4, 1, ','); // for mdk
+    return str;
+}
---
+++ Computing systems architecture/lab4/var 2/mlab 4 var 2.cpp
@@ -0,0 +1,257 @@
+#include <bits/stdc++.h>
+#include "converter.hpp"
+
+using namespace std;
+
+#define MINUS_ZERO_DOESNT_EXIST "Значения '-0' в данном коде не существует"
+
+bool int_input(string message, int& ans) {
+    bool f = false;
+    cout << message;
+    if (cin >> ans)
+        f = true;
+    else {
+        cin.clear();
+        cin.ignore();
+    }
+    return f;
+}
+char char_input(string message) {
+    bool f = false;
+    char ans = 0;
+    while (!f) {
+        cout << message;
+        if (cin >> ans)
+            f = true;
+        else {
+            cout << "Ошибка ввода\n";
+            cin.clear();
+            cin.ignore();
+        }
+    }
+    cin.clear();
+    cin.ignore();
+    return toupper(ans);
+}
+
+int main() {
+    // ипА = спА - 127
+p1:
+    int a, b, m, code_type, i_ans;
+    char response;
+    string hexA, hexB, str_ans, R;
+    bool f1, f2;
+
+    cout << "Введите два вещественных шестнадцатеричных числа:\n";
+    cout << "A = "; getline(cin, hexA);
+    cout << "B = "; getline(cin, hexB);
+    
+p2:
+    if (!isHex(hexA) || !isHex(hexB))  {
+        cout << "\nОшибка ввода исходных чисел\n\n";
+        goto p1;
+    }
+    if (hexA == "00000000" || hexB == "00000000") {
+        cout << "R(2) = 0\nR(16) = 0\n";
+        goto p1;
+    }
+
+pUnpacking:
+    string binA = hex_to_bin(hexA), binB = hex_to_bin(hexB);
+    int ipA10 = bin_to_dec(binA.substr(1, 8)) - 127;
+    int ipB10 = bin_to_dec(binB.substr(1, 8)) - 127;
+    string mA = get_matisse(binA);
+    string mB = get_matisse(binB);
+    // внимательно смотреть лишние нули
+    cout << "\n"
+         << "A(2) = " << space_wrapper(binA) << "\n"
+         << "zA = " << binA[0] << "\n"
+         << "спА(2) = " << space_wrapper(binA.substr(1, 8)) << "\n"
+         << "спА(10) = " << bin_to_dec(binA.substr(1, 8)) << "\n"
+         << "ипА(10) = " << ipA10 << "\n"
+         << "mA = " << mA << "\n"
+         << "\n"
+         << "B(2) = " << space_wrapper(binB) << "\n"
+         << "zB = " << binB[0] << "\n"
+         << "спB(2) = " << space_wrapper(binB.substr(1, 8)) << "\n"
+         << "спB(10) = " << bin_to_dec(binB.substr(1, 8)) << "\n"
+         << "ипB(10) = " << ipB10 << "\n"
+         << "mB = " << mB << "\n"
+         << "\n";
+
+p4:
+    cout << "Введите номер выполняемой операции m:\n"
+         << "1 A+B\n"
+         << "2 A-B\n" 
+         << "3 A*B\n"
+         << "4 A/B\n";
+
+p5:
+    if (!int_input("m = ", m) || m < 1 || 4 < m) {
+        cout << "\nОшибка ввода номера выполняемой операции\n\n";
+        goto p4;
+    }
+    if (m != 2) {
+        cout << "\nОперация не реализована\n\n";
+        goto p4;
+    }
+
+pOPERATION:
+
+    cout << "\n2 A-B\n";
+    string zA = to_string(binA[0] - '0');
+    string zB = to_string(binB[0] - '0');
+
+    string zR = (((binA[0] - '0') + (binB[0] - '0') == 1) ? "1" : "0");
+    cout << "zR = zA (+) zB = "
+    << zA << " (+) " << zB
+    << " = " 
+    << zR
+    << "\n\n";
+    
+
+    
+    const int sw = 12;
+    make_same_size(mA, mB, 2);
+    int mA10 = bin_to_dec(mA);
+    int mB10 = bin_to_dec(mB);
+    int force_size = mA.size() - 1;
+    
+    cout << "       А              -B\n";
+    cout << "ПК" << setw(sw) << with_comma(to_straight_code(mA10, mA10 < 0, force_size))
+         << setw(sw) << "    " << with_comma(to_straight_code(mB10, mB10 < 0, force_size)) << "\n";
+    
+    string revA = to_reverse_code(mA10, mA10 < 0, force_size), revB = to_reverse_code(mB10, mB10 < 0, force_size);
+    make_same_size(revA, revB, 2);
+    cout << "OК" << setw(sw) << with_comma(revA) << setw(sw) << "    " << with_comma(revB) << "\n";
+    
+    string extA = to_extended_code(mA10, mA10 < 0, force_size), extB = to_extended_code(mB10, mB10 < 0, force_size);
+    make_same_size(extA, extB, 2);
+    cout << "ДК" << setw(sw) << with_comma(extA)
+    << setw(sw) << "    " << with_comma(extB) << "\n";
+
+    string mA_mdk = to_mod_extended_code(mA10, mA10 < 0, force_size);
+    string mB_mdk = to_mod_extended_code(mB10, mB10 < 0, force_size);
+    make_same_size(mA_mdk, mB_mdk, 3);
+
+    cout << "МДК" << setw(sw-1) << with_comma(mA_mdk) << setw(sw-1) << "    " << with_comma(mB_mdk) << "\n";
+    cout << "\n";
+
+    // сложение порядков
+    //string mR_mdk = without_leading_zeroes(code_plus_code("0" + mA_mdk, "0"+mB_mdk));
+    /*
+    int ipR10 = ipA10 + ipB10;
+    cout << "ипA(МДК) = " << ipA_mdk << "\n";
+    cout << "ипB(МДК) = " << ipB_mdk << "\n";
+    cout << "ипR(МДК) = " << ipR_mdk << "\n";
+    string ipR2 = to_straight_code(ipR10, ipR10 < 0, 0).substr(2);
+    ipR2 = without_leading_zeroes(ipR2);
+    cout << "ипR(2) = " << ((ipR10 < 0) ? "-" : "") << (ipR2.empty() ? "0" : ipR2) << "\n"; // может быть и плюс и минус
+    cout << "ипR(10) = " << ipR10 << "\n\n"; 
+    
+    */
+pOUT:
+    // вычитание
+    const int ms = max(mA.size(), mB.size()) + mB.size();
+    int size_diff = abs((int)mA.size() - (int)mB.size());
+    // выравнивание
+    if (mA.size() > mB.size()) {
+        cout << "A " << setw(ms + size_diff) << mA << "\n"
+             << "B " << setw(ms) << mB << "\n"
+             << string(ms + 2 + size_diff, '-') << "\n";
+    }
+    else {
+        int x = 1;
+        cout << "A " << setw(ms) << mA << "\n"
+             << "B " << setw(ms + size_diff) << mB << "\n"
+             << string(ms + 2 + size_diff, '-') << "\n";
+    }
+    
+    
+    mA = mA.substr(2);
+    mB = mB.substr(2);
+    string output = zR+zR+"."+code_plus_code("0"+mA,mB+"0");
+    int mR10 = bin_to_dec(output.substr(3));
+    cout << setw(ms + 2 + size_diff) << with_comma(output) << "\n\n";
+    
+    
+    cout << "mR = " << with_comma(output) << "\n";
+    if (zR == "1") {
+        cout << "R(МДК) = " << with_comma(to_mod_extended_code(mR10, mR10 < 0, force_size)) << "\n";
+        cout << "R(ДК) = " << with_comma(to_extended_code(mR10, mR10 < 0, force_size)) << "\n";
+        cout << "R(ОК) = " << with_comma(to_reverse_code(mR10, mR10 < 0, force_size)) << "\n";
+        cout << "R(ПК) = " << with_comma(to_straight_code(mR10, mR10 < 0, force_size)) << "\n";
+    }
+    
+    int spR10 = max(ipA10, ipB10) + 127;
+    cout << "спR(2) = " << to_straight_code(spR10, spR10 < 0, force_size).substr(2) << "\n";
+    cout << "спR(10) = " << spR10 << "\n";
+
+
+    string mR = to_straight_code(mR10, mR10 < 0, force_size).substr(2);
+    int leading_zeros = 0;
+    while (leading_zeros < mR.size() && mR[leading_zeros] == '0')
+        leading_zeros++;
+
+    string R2 = zR + to_straight_code(spR10, spR10 < 0, force_size).substr(2) + mR.substr(leading_zeros);
+    int diff = 32 - R2.size();
+    if (diff > 0)
+        R2 += string(diff, '0');
+
+    cout << "R(2) = " << space_wrapper(R2) << "\n";
+    cout << "R(16) = " << space_wrapper(bin_to_hex(R2)) << "\n";
+    
+p7:
+    // Если требуется нормализация, то выводим
+ /*   bool need_normalization = output[0] == '0';
+    int leading_zeros = 0;
+    while (leading_zeros < output.size() && output[leading_zeros] == '0')
+        leading_zeros++;
+
+    cout << "\n";
+    if (need_normalization) // не учитывается сдвиг в другую сторону (нет примеров)
+        cout << "mRн = " << "0," + output.substr(leading_zeros) << "\n"
+            << "ипRн(10) = " << ipR10 + leading_zeros << "\n";
+
+    int spR10 = ipR10 + 127 - leading_zeros; // leading_zeros - normalisation
+    bitset<8> bst_spr(spR10);
+    string spR2 = bst_spr.to_string();
+    string R2 = zR + spR2 + output.substr(leading_zeros);
+    int diff = 32 - R2.size();
+    if (diff > 0)
+        R2 += string(diff, '0');
+    string R16 = bin_to_hex(R2);
+
+    cout << "спR(10) = " << spR10 << "\n"
+         << "спR(2) = " << spR2 << "\n"
+         << "R(2) = " << space_wrapper(R2) << "\n"
+         << "R(16) = " << space_wrapper(R16) << "\n";
+*/
+p8:
+    cout << "Изменить значения исходных чисел?\n";
+    response = char_input("(Y - да / N - нет)\n");
+    switch (response)
+    {
+    case 'Y':
+        goto p1;
+    case 'N':
+        break;
+    default:
+        cout << "Ошибка ввода запроса на изменение исходных чисел\n";
+        goto p8;
+    }
+p9:
+    cout << "Завершить работу программы?\n";
+    response = char_input("(Y - да / N - нет)\n");
+    switch (response)
+    {
+    case 'Y':
+        break;
+    case 'N':
+        goto p1;
+    default:
+        cout << "Ошибка ввода запроса на завершение программы\n";
+        goto p9;
+    }
+    return 0;
+}
