f9c8169704d80f5b5c8251edb4c1f8a18850d77c
viCodexm <uinkognito@yandex.ru>
2023-12-10 16:11:28 +0300

    fully functional PvE and PvP with animations. almost a release version

--- Algorithms and data structures/Finy - The Board Game/Board.hpp
+++ Algorithms and data structures/Finy - The Board Game/Board.hpp
@@ -5,6 +5,12 @@
 using namespace std;
 
 struct Board {
+    struct SmoothMoveAnimator {
+        bool isMoving = false;
+        int row, from, to;
+        sf::Clock startTimeStamp;
+    };
+    SmoothMoveAnimator moveAnimator;
     vector<vector<ReactiveRectangle>> cells;
     WinnerBoard winnerboard;
     GameModeBoard gameModeBoard;
@@ -15,7 +21,7 @@ struct Board {
     int pressed_idx = -1;
     int ready_to_move_idx = -1;
     int player_move = 1;
-    int win = false, restart = false;
+    int win = false, restart = false, set_once = false;
     
     Board() {
         buffer.loadFromFile("long-pop.wav");
@@ -32,16 +38,120 @@ struct Board {
                 cells[i][j] = cell;
             }
         }
-
         for (int idx : {2, 4, 6}) {
             pair<int, int> p = getCoordinates(idx);
             cells[p.first][p.second].isGamePiecePlayer = true;
         }
-        
         for (int idx : {15, 19, 23}) {
             pair<int, int> p = getCoordinates(idx);
             cells[p.first][p.second].isGamePieceEnemy = true;
         }
+    }
+
+    void logic(sf::RenderWindow& window) {
+        if (gameModeBoard.gamemode == -1) {
+            gameModeBoard.logic(window);
+            return;
+        }
+        if (!win)
+            play_the_game(window);
+        else if (!set_once) {
+            set_once = true;
+            sf::sleep(sf::milliseconds(1000));
+            winnerboard.set_message(win == 1 ? "Red player won!" : "Blue player won!"
+                                    , win == 1 ? COLOR_RED : COLOR_BLUE);
+        }
+        
+        winnerboard.logic(window);
+        restart = winnerboard.retry.pressed;
+    }
+
+    void play_the_game(sf::RenderWindow& window) {
+        if (moveAnimator.isMoving) {
+            if (moveAnimator.startTimeStamp.getElapsedTime().asMilliseconds() > 60) {
+                moveAnimator.startTimeStamp.restart();
+                // with this buttons change colors mid animation
+                for (auto& row : cells)
+                    for (ReactiveRectangle& cell : row) {
+                        cell.pressed = false;
+                        cell.logic(window, player_move);
+                    }
+                move_process();
+            }
+        }
+        else {
+            if (gameModeBoard.gamemode == 2 && !player_move)
+                enemy_logic(window);
+            else player_logic(window);
+        }
+        for (auto& row : cells)
+            for (ReactiveRectangle& cell : row)
+                window.draw(cell);
+    }
+    
+    // Enemy and Player
+    
+    void enemy_logic(sf::RenderWindow& window) {
+        sf::sleep(sf::milliseconds(1000));
+        bot_make_move(window);
+    }    
+
+    void player_logic(sf::RenderWindow& window) {
+        int idx = 0;
+        for (auto& row : cells)
+            for (ReactiveRectangle& cell : row) {
+                cell.pressed = (idx == pressed_idx);
+                cell.logic(window, player_move);
+                if (cell.pressed) {
+                    pressed_idx = idx;
+                    if (cell.isGamePieceEnemy || cell.isGamePiecePlayer)
+                        ready_to_move_idx = idx;
+                }   
+                auto [a, b] = getCoordinates(ready_to_move_idx);
+                bool needHints = cell.pressed && ((cell.isGamePieceEnemy && !player_move) || (cell.isGamePiecePlayer && player_move));
+                if (needHints)
+                    add_hints(a, b);
+
+                if (ready_to_move_idx == idx) {
+                    idx++;
+                    continue;
+                }
+                    
+                auto [x, y] = getCoordinates(idx);
+                bool moveBelongsToPlayer = ((cells[a][b].isGamePieceEnemy && !player_move) || (cells[a][b].isGamePiecePlayer && player_move));
+                bool moveToEmptyCell = !cell.isGamePieceEnemy && !cell.isGamePiecePlayer;
+                bool sameRow = b == y;
+
+                if (moveBelongsToPlayer && moveToEmptyCell && cells[x][y].hint && cell.pressed && ready_to_move_idx != -1 && sameRow)
+                    init_smooth_moveAnimation(a, x, b);
+                
+                idx++;
+            }
+    }
+
+
+
+// maybe add option to choose who to play vs bot
+// perfect state
+private:
+    void add_hints(int x, int y) {
+        for (auto& [i, j] : marked)
+            cells[i][j].hint = false;
+
+        marked.clear();
+        for (int i = x - 1; i >= 0; --i) {
+            if (cells[i][y].isGamePieceEnemy || cells[i][y].isGamePiecePlayer)
+                break;
+            marked.push_back({i, y});
+        }
+        for (int i = x + 1; i < 8; ++i) {
+            if (cells[i][y].isGamePieceEnemy || cells[i][y].isGamePiecePlayer)
+                break;
+            marked.push_back({i, y});
+        }
+
+        for (auto& [i, j] : marked)
+            cells[i][j].hint = true;
     }
     vector<int> get_positions() {
         vector<int> positions;
@@ -79,6 +189,10 @@ struct Board {
             return 1;
         return 0;
     }
+    pair<int, int> getCoordinates(int idx) {
+        return {idx / 3, idx % 3};
+    }
+
     void bot_make_move(sf::RenderWindow& window) {
         vector<int> p = get_positions();
         int dist1 = abs(p[0] - p[1]) - 1, dist2 = abs(p[2] - p[3]) - 1, dist3 = abs(p[4] - p[5]) - 1;
@@ -107,96 +221,50 @@ struct Board {
                     else if (dist2 == cur[j] - '0') j = 1;
                     else j = 2;
                     // enemy - red
-                    swap(cells[p[2*j+1]][j].isGamePieceEnemy, cells[p[2*j+1] - offset][j].isGamePieceEnemy);
+                    init_smooth_moveAnimation(p[2*j+1], p[2*j+1]-offset, j);
+                    
                     // enemy - blue
-                    //swap(cells[p[2*j]][j].isGamePiecePlayer, cells[p[2*j] + offset][j].isGamePiecePlayer);
+                    //init_smooth_moveAnimation(p[2*j], p[2*j]+offset, j);
                     return;
                 }
             }
         }
     }
 
-    void logic(sf::RenderWindow& window) {
-        if (gameModeBoard.gamemode == -1) {
-            gameModeBoard.logic(window);
-            return;
-        }
-        int idx = 0;
-        if (!win) {
-            if (gameModeBoard.gamemode == 2 && !player_move) {
-                bot_make_move(window);
-                popSound.play();
-                        
-                pressed_idx = -1;
-                player_move = !player_move;
-                win = check_win();
-                sf::sleep(sf::milliseconds(50));
-            }
-            for (auto& row : cells)
-                for_each(row.begin(), row.end(), [&](ReactiveRectangle& cell) {
-                    cell.pressed = (idx == pressed_idx);
-                    cell.logic(window, player_move);
-                    if (cell.pressed)
-                        pressed_idx = idx;
-                    
-                    if ((cell.isGamePieceEnemy || cell.isGamePiecePlayer) && cell.pressed) {
-                        ready_to_move_idx = idx;   
-                    }
-                    
-                    pair<int, int> a = getCoordinates(ready_to_move_idx);
-                    pair<int, int> b = getCoordinates(idx);
-                    if (cells[b.first][b.second].hint && ((cells[a.first][a.second].isGamePieceEnemy && !player_move) || (cells[a.first][a.second].isGamePiecePlayer && player_move))
-                    && !cell.isGamePieceEnemy && !cell.isGamePiecePlayer && cell.pressed && ready_to_move_idx != -1 && a.second == b.second) {
-                        player_move ?
-                            swap(cells[a.first][a.second].isGamePiecePlayer, cells[b.first][b.second].isGamePiecePlayer)
-                            :swap(cells[a.first][a.second].isGamePieceEnemy, cells[b.first][b.second].isGamePieceEnemy);
-                        
-                        popSound.play();
-                        
-                        pressed_idx = -1;
-                        player_move = !player_move;
-                        win = check_win();
-                        
-                        for (auto& [i, j] : marked)
-                            cells[i][j].hint = false;
-                        marked.clear();
-                        sf::sleep(sf::milliseconds(50));
-                    }
-                    
-                    if (cell.pressed && ((cell.isGamePieceEnemy && !player_move) || (cell.isGamePiecePlayer && player_move))) {
-                        for (auto& [i, j] : marked)
-                            cells[i][j].hint = false;
-
-                        marked.clear();
-                        for (int i = b.first - 1; i >= 0; --i) {
-                            if (cells[i][b.second].isGamePieceEnemy || cells[i][b.second].isGamePiecePlayer)
-                                break;
-                            marked.push_back({i, b.second});
-                        }
-                        for (int i = b.first + 1; i < 8; ++i) {
-                            if (cells[i][b.second].isGamePieceEnemy || cells[i][b.second].isGamePiecePlayer)
-                                break;
-                            marked.push_back({i, b.second});
-                        }
-
-                        for (auto& [i, j] : marked)
-                            cells[i][j].hint = true;
-                    }
-
-                    window.draw(cell);
-                    idx++;
-                });
-        }
-        else if (win == 1)
-            winnerboard.set_message("Red player won!", COLOR_RED);
-        else if (win == 2)
-            winnerboard.set_message("Blue player won!", COLOR_BLUE);
-        
-        winnerboard.logic(window);
-        restart = winnerboard.retry.pressed;
+    void after_move_cleanup() {
+        pressed_idx = -1;
+        player_move = !player_move;
+        for (auto& [i, j] : marked)
+            cells[i][j].hint = false;
+        marked.clear();
     }
-private:
-    pair<int, int> getCoordinates(int idx) {
-        return {idx / 3, idx % 3};
+    void swap_em(int a, int b, int x, int y) {
+        swap(cells[a][b].isGamePiecePlayer, cells[x][y].isGamePiecePlayer);
+        swap(cells[a][b].isGamePieceEnemy, cells[x][y].isGamePieceEnemy);
+    }
+    void move_process() {
+        auto& [isMoving, row, from, to, timeStamp] = moveAnimator;
+        if (from == to) {
+            isMoving = false;
+            win = check_win();
+        }
+        else {
+            // to Up
+            if (from > to) {
+                swap_em(from, row, from-1, row);
+                from--;
+            } else {
+                swap_em(from, row, from+1, row);
+                from++;
+            }
+        }
+    }
+    void init_smooth_moveAnimation(int from, int to, int row) {
+        popSound.play();
+        moveAnimator.startTimeStamp.restart();
+        moveAnimator.isMoving = true;
+        moveAnimator.from = from; moveAnimator.to = to;
+        moveAnimator.row = row;
+        after_move_cleanup();
     }
 };
