b37aca142239d94d3102b3ca44c25391829088e2
viCodexm <uinkognito@yandex.ru>
2024-03-01 11:04:01 +0300

    add akkerman

--- semester-4/Data processing structures and algorithms/lab 2. Tree/main.cpp
+++ semester-4/Data processing structures and algorithms/lab 2. Tree/main.cpp
@@ -54,6 +54,7 @@ struct BalancedTree {
         if (!cur) return NULL;
         return 1 + countNodes(cur->left) + countNodes(cur->right);
     }
+
     bool isValidBST() {
         return isValidBST(root, LONG_MIN, LONG_MAX);
     }
@@ -103,6 +104,7 @@ struct BalancedTree {
     }
 };
 struct Solution {
+    
     void solve() {
         cout << "Формирование идеально сбалансированного дерева:\n";
         //cout << "Введите n: "; int n; cin >> n;
@@ -116,13 +118,34 @@ struct Solution {
         int layer = 2;
         cout << "Сумма ключей на уровне " << layer << ": " << bt.layerSum(layer) << "\n";
     }
+    void akkerman() {
+        int m = 3, n = 10;
+        cout << "Для аргументов " << m << " " << n << "\n";
+        cout << "Ответ: " << a(m, n) << "\n";
+    }
+    map<pair<int, int>, int> cache;
+
+    int a(int m, int n) {
+        if (m == 0)
+            return cache[{m, n}] = n + 1;
+
+        if (cache.find({m, n}) != cache.end())
+            return cache[{m, n}];
+
+        if (n == 0)
+            return cache[{m, n}] = a(m - 1, 1);
+
+        if (n > 0)
+            return cache[{m, n}] = a(m - 1, a(m, n - 1));
+    }
 };
 
 int main() {
     freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
     Solution a;
-    a.solve();
+    //a.solve();
+    a.akkerman();
 
     return 0;
 }
